4d756e796988117da7b1502759338c0191ed9bd9
rajatlko13 <rajatlko13@gmail.com>
2021-10-30 12:53:03 +0530

    First commit

---
+++ src/main/java/com/rajat/hackAPI/HackApiApplication.java
@@ -0,0 +1,13 @@
+package com.rajat.hackAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackApiApplication.class, args);
+	}
+
+}
